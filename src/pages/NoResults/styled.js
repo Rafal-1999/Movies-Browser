6b9a507facfe8d19042dec79799f0005c973b2cf
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2024-06-13 18:34:31 +0200

    added styled in the NoResults folder

---
+++ src/pages/NoResults/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { ReactComponent as NoResultsImage } from '../../assets/images/noResults.svg';
+import { device } from '../../styles/breakpoints';
+
+export const Container = styled.div`
+    padding: 20px;
+
+    @media ${device.md} {
+        padding: 2px;
+    }
+
+    @media ${device.xl} {
+        padding: 32px;
+    }
+`;
+
+export const Header = styled.h2`
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 18.2px;
+    margin: 40px;
+
+    @media ${device.sm} {
+        font-size: 36px;
+        font-weight: 600;
+        line-height: 43.2px;
+    }
+`;
+
+export const Image = styled(NoResultsImage)`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+
+  @media ${device.xs} {
+        width: 90%;
+  }
+`;
