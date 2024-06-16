03814ad78ec22b37dafefacf205e2df7e2039448
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2024-06-16 18:37:21 +0200

    added components in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-const App = () => {
-    return (<></>)
-};
+import NoResults from "./components/NoResults";
+import Error from "./components/Error";
+import Loading from "./components/Loading";
+
+const App = () => (
+  <>
+   <NoResults />
+    <Error />
+   <Loading />
+  </>
+);
 
 export default App;
